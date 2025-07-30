2112b16588a64b1a81a81c45d3cc9d2d3cb91faf
Tanja private <tanja120a@gmail.com>
2025-07-30 07:20:05 +0200

    revert experimental ppr

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    ppr: 'incremental'
-  }
+  /* config options here */  
 };
 
 export default nextConfig;
